060e70b12e5e19e09da6c7bf03db1a7a891de4a4
German-Zero <germannaz_@hotmail.com>
2026-08-14 02:07:06 -0300

    effects: borders reestructurado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 import "@/features/rewards/styles/surfaces.css";
+import "@/features/rewards/effects/borders/borders.css";
 
 const sora = Sora({
   subsets: ["latin"],
